64592cb7f8247e6d0b8032340fb02f1e69e370d7
PodTT <82187910+PodTT@users.noreply.github.com>
2023-05-30 10:39:12 -0700

    Update many.py

--- many.py
+++ many.py
@@ -18,7 +18,7 @@ for video_file in video_files:
     output_file = os.path.join(output_directory, video_name + ".mp4")
 
     # Run the roop script with the appropriate command
-    command = f"python run.py -f {face_image} -t {video_file} -o {output_file}"
+    command = f"python run.py --gpu -f {face_image} -t {video_file} -o {output_file}"
     subprocess.call(command, shell=True)
 
     print(f"Processed video: {video_file}")
